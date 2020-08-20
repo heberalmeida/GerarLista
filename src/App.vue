d87dc9aa710ba2491758911c054eb17a1285b4b9
Heber Almeida <eu@heber.com.br>
2020-08-20 15:18:57 -0400

    add ads

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <Adsense data-ad-client="ca-pub-9214659967456120" data-ad-slot="1234567890">
+    </Adsense>
     <img src="./assets/lista.png" border="0" />
     <form @submit.prevent="getGerar">
       <ul>
@@ -123,6 +125,7 @@
         </button>
       </div>
     </form>
+    <Adsense data-ad-client="ca-pub-9214659967456120" data-ad-slot="1234567890">
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueClipboard from 'vue-clipboard2'
+import Ads from 'vue-google-adsense'
 
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueClipboard)
+Vue.use(require('vue-script2'))
+Vue.use(Ads.Adsense)
+Vue.use(Ads.InArticleAdsense)
+Vue.use(Ads.InFeedAdsense)
 
 new Vue({
   render: h => h(App)
